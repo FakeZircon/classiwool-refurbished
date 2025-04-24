f97b86c347896b424a3745bd8ce050e5a00671e9
FakeZircon <102265454+FakeZircon@users.noreply.github.com>
2025-04-24 14:05:40 -0600

    mod template added

---
+++ src/client/java/classiwool_refurbished/ClassiwoolRefurbishedDataGenerator.java
@@ -0,0 +1,11 @@
+package classiwool_refurbished;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClassiwoolRefurbishedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
